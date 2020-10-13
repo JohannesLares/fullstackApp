f918f63ff2fe69a2c5ae7dd991ff441274c56d0e
johanneslares <Johq.lares@gmail.com>
2020-10-13 16:53:27 +0300

    Osa2 again v2

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { TextInput as NativeTextInput } from 'react-native';
+import { TextInput as NativeTextInput, StyleSheet } from 'react-native';
+import theme from '../styles/theme';
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: theme.fonts.main,
+  }
+});
 
 const TextInput = ({ style, error, ...props }) => {
   let textInputStyle = [style];
@@ -7,7 +14,13 @@ const TextInput = ({ style, error, ...props }) => {
   if (error) {
     textInputStyle = {
       ...style,
-      borderColor: "#eb4034"
+      borderColor: "#eb4034",
+      ...styles.text
+    };
+  } else {
+    textInputStyle = {
+      ...style,
+      ...styles.text
     };
   }
 
